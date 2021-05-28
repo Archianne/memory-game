b551eacd0f43745c001164d36b1d6914ca7efd1a
Archianne <helena19w@gmail.com>
2021-05-28 20:04:59 +0100

    added images to array

---
+++ main.js
@@ -0,0 +1,66 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    //card options
+const cardArray = [
+    {
+        name: 'chicken',
+        img: 'img/chicken.png'
+    },
+    {
+        name: 'chicken',
+        img: 'img/chicken.png'
+    },
+    {
+        name: 'fish',
+        img: 'img/fish.png'
+    },
+    {
+        name: 'fish',
+        img: 'img/fish.png'
+    },
+    {
+        name: 'lion',
+        img: 'img/lion.png'
+    },
+    {
+        name: 'lion',
+        img: 'img/lion.png'
+    },
+    {
+        name: 'monkey',
+        img: 'img/monkey.png'
+    },
+    {
+        name: 'monkey',
+        img: 'img/monkey.png'
+    },
+    {
+        name: 'octopus',
+        img: 'img/octopus.png'
+    },
+    {
+        name: 'octopus',
+        img: 'img/octopus.png'
+    },
+    {
+        name: 'turtle',
+        img: 'img/turtle.png'
+    },
+    {
+        name: 'turtle',
+        img: 'img/turtle.png'
+    }
+]
+
+    const grid = document.querySelector('.grid');
+
+
+
+
+
+
+
+
+
+
+});
